aeb89f9671adb9a4013f69f7defef7e7dbc8b121
Rooster <othmanmuhidean@gmail.com>
2020-01-04 12:30:26 -0800

    a couple of keys added in

---
+++ script.js
@@ -0,0 +1,13 @@
+var edamkey = "2e68c484c12e4ddcf68cd4c939cef6e0";
+var edamURL = "https://api.edamam.com/search?q=";
+
+var spnkey = "&apiKey=3f49daeedf3244208518bde7bf5fe0fd";
+var spnURL = "https://api.spoonacular.com/food/products/search?query="
+
+var homnivore = "";
+
+var carnivore = "";
+
+var vegetarian = "";
+
+var vegan = "";
